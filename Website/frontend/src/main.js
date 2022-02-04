4a6ab1873412a1e78497182607e2454959f57148
cmtnguyen <66109485+cmtnguyen@users.noreply.github.com>
2022-02-03 18:56:56 -0800

    added nav & footer

--- Website/frontend/src/main.js
+++ Website/frontend/src/main.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './public/main.css';
-import Navigation from './components/navbar.js';
+import Navigation from './components/navbar';
+import Footer from './components/footer';
 
-function Main(){
-    return(
+function Main() {
+    return (
         <div>
-            <head>
-                <title>AFCPlushies</title>
-                <link rel="icon" href="/favicon.ico"></link>
-            </head>
-            <div>
+            <nav>
                 <Navigation />
-            </div>
+            </nav>
             <div>
                 <h1>AFCPlushies</h1>
                 <h2>ROugh DrAft</h2>
             </div>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 }
