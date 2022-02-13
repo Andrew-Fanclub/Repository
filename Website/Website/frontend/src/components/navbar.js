241862e5dcd9581d44fbe8813fb934941a6299a5
4-gent <40777454+4-gent@users.noreply.github.com>
2022-02-13 00:59:46 -0800

    Add files via upload

--- Website/Website/frontend/src/components/navbar.js
+++ Website/Website/frontend/src/components/navbar.js
@@ -1,22 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import React from 'react';
 import "../public/nav.css";
-import Logo from "../images/Logo.png";
+import "../routes/howWorks";
+import "../routes/whoWe";
+import "../routes/contact";
 
 function Navigation() {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container fluid>
-                    <Navbar.Brand href="/"><img src={Logo} alt="logo" /></Navbar.Brand>
+                    <Navbar.Brand href="/">AFCPlushies</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/howitworks" className="navi">How it works</Nav.Link>
+                            <Nav.Link href="/howitWorks" className="navi">How it works</Nav.Link>
                             <span className="dots">&bull;</span>
-                            <Nav.Link href="/aboutus" className="navi">Who we are</Nav.Link>
+                            <Nav.Link href="/whoweAre" className="navi">Who we are</Nav.Link>
                             <span className="dots">&bull;</span>
-                            <Nav.Link href="/contact" className="navi">Contact us</Nav.Link>
+                            <Nav.Link href="/contactUs" className="navi">Contact us</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
